7c9ff17814c6d659b36919b00d9ea76fad39a4d1
Alan <fkdrsp@proton.me>
2023-07-25 20:58:59 +0200

    feat. using swr to fetch menu

--- components/HomeView.jsx
+++ components/HomeView.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import useSWR from "swr";
+import { fetcher } from "../utils/fetcher";
+
 import { View, Text, SafeAreaView } from "react-native";
 
 import Header from "./Header";
 
 import { API_ENDPOINTS } from "../utils/config";
 
-const getMenu = async () => {};
-
 const HomeView = () => {
+	const { data, error, isLoading } = useSWR(API_ENDPOINTS.MENU, fetcher);
+
+	if (error) return <Text>Something went wrong</Text>;
+
+	if (isLoading) return <Text>Loading...</Text>;
+	console.log(data);
 	return (
 		<SafeAreaView>
 			<Header />
