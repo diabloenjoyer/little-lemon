7fad33e15e2b89ef6386eec68a583cfa8ab826fd
Alan <fkdrsp@proton.me>
2023-07-26 12:17:29 +0200

    feat. menu item image api link

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,6 @@
 export const API_ENDPOINTS = {
 	MENU: "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/capstone.json",
+	MENU_IMAGE(imageFileName) {
+		return `https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/${imageFileName}?raw=true`;
+	},
 };
