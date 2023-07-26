4c41eaae3712b38e109fd9403eb286cd7bfd9c2e
Alan <fkdrsp@proton.me>
2023-07-26 14:19:50 +0200

    feat. added brand colors

--- utils/config.js
+++ utils/config.js
@@ -4,3 +4,29 @@ export const API_ENDPOINTS = {
 		return `https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/${imageFileName}?raw=true`;
 	},
 };
+
+export const COLORS = {
+	apple: { blue: "#007AFF" },
+	brand: {
+		green: "#495E57",
+		yellow: "#F4CE14",
+		coral: "#EE8872",
+		lightHighlight: "#EDEFEE",
+		darkHighlight: "#333333",
+	},
+};
+
+const FONTS = {
+	MARAKAZI: "Marakazi Text",
+	KARLA: "Karla Regular",
+};
+export const brandFont = (textType = "default") => {
+	switch (textType) {
+		case "heading":
+		case "sentence-styled":
+		case "leading":
+			return FONTS.MARAKAZI;
+		default:
+			return FONTS.KARLA;
+	}
+};
